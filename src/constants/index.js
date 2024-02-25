eef7090f5d9f40bb2b9ca78aefff43e1dda3b814
younus123shaik <younus123shaik@gmail.com>
2024-02-26 00:38:39 +0530

    f

--- src/constants/index.js
+++ src/constants/index.js
@@ -61,14 +61,7 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
+   
     {
       name: "JavaScript",
       icon: javascript,
